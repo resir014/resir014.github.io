da00ba2950f82766b7e2cce91849d85e353b722d
Resi Respati <resir014@gmail.com>
2014-09-15 15:35:39 +0700

    Added Grunt script for code cleaning & livereload.

---
+++ Gruntfile.js
@@ -0,0 +1,76 @@
+module.exports = function (grunt) {
+    // load all grunt tasks matching the `grunt-*` pattern
+    require('load-grunt-tasks')(grunt);
+
+    grunt.initConfig({
+
+      autoprefixer: {
+        options: {
+          browsers: ['> 1%', 'last 2 versions', 'Firefox ESR', 'Opera 12.1']
+        },
+        build: {
+          src: 'public/css/style.css'
+        }
+      },
+
+      csscomb: {
+        options: {
+          config: 'public/css/.csscomb.json'
+        },
+        build: {
+          src: 'public/css/style.css',
+          dest: 'public/css/style.css'
+        }
+      },
+
+      shell: {
+        jekyll: {
+          command: 'jekyll build'
+        }
+      },
+
+      watch: {
+        options: {
+          livereload: true
+        },
+        jekyllSources: {
+          files: [
+            '*.html', '*.md', '*.yml', 'public/css/**.css', '_posts/**',
+            '_includes/**', 'atom.xml', '**/*.md'
+          ],
+          tasks: ['shell:jekyll']
+        }
+      },
+
+      connect: {
+        server: {
+          options: {
+            base: '_site/',
+            port: 9001
+          }
+        }
+      },
+      open: {
+        server: {
+          path: 'http://localhost:<%= connect.server.options.port %>/'
+        }
+      }
+
+    });
+
+    grunt.registerTask('build', [
+      'autoprefixer:build', 'csscomb:build'
+    ]);
+
+    grunt.registerTask('server', [
+      'connect:server',
+      'open:server',
+      'watch'
+    ]);
+
+    // livereload task
+    grunt.registerTask('serve', ['server']);
+
+    // default task
+    grunt.registerTask('default', 'build');
+}
