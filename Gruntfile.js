677666fc3090f9052614c938c0ecc4653f9de817
Resi Respati <resir014@gmail.com>
2015-11-21 19:31:22 +0700

    Added Grunt tasks

---
+++ Gruntfile.js
@@ -0,0 +1,59 @@
+module.exports = function(grunt) {
+
+  // Load all tasks matching the `grunt-*` pattern
+  require('load-grunt-tasks')(grunt);
+
+  grunt.initConfig({
+    // Project configuration
+    pkg: grunt.file.readJSON('package.json'),
+
+    // Handle vendor prefixing
+    postcss: {
+      options: {
+        processors: [
+          require('autoprefixer')({ browsers: ['last 2 versions', 'ie 8', 'ie 9'] })
+        ]
+      },
+      publish: {
+        src: '_site/*.css'
+      }
+    },
+
+    jekyll: {
+      options: {
+        bundleExec: true,
+        src : '<%= app %>'
+      },
+      dist: {
+        options: {
+          dest: '<%= dist %>',
+          config: '_config.yml'
+        }
+      },
+    },
+
+    buildcontrol: {
+      options: {
+        dir: '_site',
+        commit: true,
+        push: true,
+        message: 'Built %sourceName% from commit %sourceCommit% on branch %sourceBranch%'
+      },
+      publish: {
+        options: {
+          remote: 'git@github.com:resir014/resir014.github.io.git',
+          branch: 'master'
+        }
+      }
+    }
+  });
+
+  // Default task
+  grunt.registerTask('default', ['build']);
+
+  // Manually build page
+  grunt.registerTask('build', ['jekyll', 'postcss']);
+
+  // Publish to GitHub
+  grunt.registerTask('publish', ['jekyll', 'postcss', 'buildcontrol']);
+}
