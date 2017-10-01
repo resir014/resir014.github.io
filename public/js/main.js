0a9a82e8f537fdf89853714e7f1476f225590bcd
Resi Respati <resir014@gmail.com>
2017-10-01 15:15:16 +0700

    updated splash texts

--- public/js/main.js
+++ public/js/main.js
@@ -1,26 +1,28 @@
 ;(function () {
   var flavors = [
-    'has an internet connection',
-    'makes stupid web things',
+    'is a web developer',
     'writes code',
     'drives an Asp Explorer',
-    'stands up for net neutrality',
     'is online',
+    'is offline',
+    'is typing',
     'plays video games',
     'has a phone',
     'takes photos',
     'is afraid of dogs',
     'hates TV',
-    'dumps abandoned projects on GitHub',
+    'likes vaporwave',
+    'does React',
     'does TypeScript',
     'does JavaScript',
+    'does node.js',
     'listens to music',
     'has a laptop',
-    'is a web developer',
+    'wrote this flavor text',
+    'has an internet connection',
     'sleeps at night',
     'is not a furry',
-    'probably likes you',
-    'wants to be your friend'
+    'probably likes you'
   ]
 
   // select our random flavor text from the above array
